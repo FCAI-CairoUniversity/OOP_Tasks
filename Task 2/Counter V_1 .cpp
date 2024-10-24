e26545bb8946f15f116ddd252be66804655b316a
mohamedahmed2005 <162487393+mohamedahmed2005@users.noreply.github.com>
2024-10-24 12:23:29 +0300

    Create Counter V_1 .cpp

---
+++ Task 2/Counter V_1 .cpp
@@ -0,0 +1,52 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+
+class Counter
+{
+	protected:
+			int count;
+	public:
+			Counter() //default constructor
+      {
+				count=0;
+			}
+			Counter(int c) //constructor with parameter
+      {
+				count=c;
+			}
+int getCount ()
+{
+	return count;
+}
+void increment()
+  {
+	count++; 	
+  }
+} ;
+class CounterDec : public Counter
+{
+	public:
+		void decrement()
+		{
+			count--;
+		}
+};
+
+
+int main(int argc, char *argv[])
+{
+    CounterDec cd;
+	cout<< cd.getCount();  //0
+	cd.increment();  //register a student  
+	cd.increment(); //register another student
+	cd.increment(); //register another student
+	cout<< cd.getCount();  //3
+	cd.decrement();  //cancel the registration of a student  
+	cd.decrement(); // cancel the registration of another student
+	cout<< cd.getCount();  //1
+    system("PAUSE");
+    return 0;
+}
